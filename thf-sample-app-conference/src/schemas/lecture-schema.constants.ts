944dc5f8d6cd9d600e7d6ca358e24ebfb1437652
Nicole Oliveira da Silva <nicole.silva@totvs.com.br>
2018-06-22 14:48:55 -0300

    #5216 Adicionado urls de diff nos schemas

--- thf-sample-app-conference/src/schemas/lecture-schema.constants.ts
+++ thf-sample-app-conference/src/schemas/lecture-schema.constants.ts
@@ -2,7 +2,7 @@ import { ThfSyncSchema } from '@totvs/thf-sync';
 
 export const lectureSchema: ThfSyncSchema = {
   getUrlApi: 'http://localhost:8080/conference-api/api/v1/lectures',
-  diffUrlApi: 'http://localhost:8080/conference-api/api/v1/lectures',
+  diffUrlApi: 'http://localhost:8080/conference-api/api/v1/lectures/diff',
   deletedField: 'deleted',
   fields: [ 'id', 'title', 'room', 'startTime', 'endTime', 'description', 'track', 'speaker' ],
   idField: 'id',
--- thf-sample-app-conference/src/schemas/user-schema.constants.ts
+++ thf-sample-app-conference/src/schemas/user-schema.constants.ts
@@ -2,7 +2,7 @@ import { ThfSyncSchema } from '@totvs/thf-sync';
 
 export const userSchema: ThfSyncSchema = {
   getUrlApi: 'http://localhost:8080/conference-api/api/v1/users',
-  diffUrlApi: 'http://localhost:8080/conference-api/api/v1/users',
+  diffUrlApi: 'http://localhost:8080/conference-api/api/v1/users/diff',
   deletedField: 'deleted',
   fields: [
     'id',
