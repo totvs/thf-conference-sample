47fbcec1bc8286512bea00b32802e229ee1e54d1
Nicole Oliveira da Silva <nicole.silva@totvs.com.br>
2018-06-22 15:12:45 -0300

    #5216 Adicionado sync reativo em Lecture Detail

--- thf-sample-app-conference/src/pages/lecture-detail/lecture-detail.ts
+++ thf-sample-app-conference/src/pages/lecture-detail/lecture-detail.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
-import { NavParams } from 'ionic-angular';
+import { NavParams, Refresher } from 'ionic-angular';
+
+import { ThfSyncService } from '@totvs/thf-sync';
 
 import { LectureService } from '../../services/lecture.service';
 
@@ -12,9 +14,19 @@ export class LectureDetailPage {
 
   lecture;
 
-  constructor(public navParams: NavParams, private lectureService: LectureService) {}
+  constructor(public navParams: NavParams, private lectureService: LectureService, private thfSync: ThfSyncService) {
+    this.thfSync.onSync().subscribe(() => this.loadLecture());
+  }
 
   ionViewWillEnter() {
+    this.loadLecture();
+  }
+
+  doRefresh(refresher: Refresher) {
+    this.lectureService.synchronize().then(() => refresher.complete());
+  }
+
+  private loadLecture() {
     this.lectureService.getLecture(this.navParams.data.lectureId).then(lecture => {
       this.lecture = lecture;
     });
