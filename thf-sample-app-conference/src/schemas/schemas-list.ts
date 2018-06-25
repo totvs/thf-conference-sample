956a8e5be26dbaef9c0df5a0edb3468a6c87297c
Nicole Oliveira da Silva <nicole.silva@totvs.com.br>
2018-06-25 16:49:00 -0300

    #5248 Adicionado Note Schema

--- thf-sample-app-conference/src/schemas/schemas-list.ts
+++ thf-sample-app-conference/src/schemas/schemas-list.ts
@@ -1,10 +1,12 @@
 import { lectureSchema } from './lecture-schema.constants';
+import { noteSchema } from './note-schema.constants';
 import { speakerSchema } from './speaker-schema.constants';
 import { trackSchema } from './track-schema.constants';
 import { userSchema } from './user-schema.constants';
 
 export const schemas = [
   lectureSchema,
+  noteSchema,
   speakerSchema,
   trackSchema,
   userSchema,
