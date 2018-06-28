7f3546c9af30902aed4e7453e3b7d99cc0a4f918
Nicole Oliveira da Silva <nicole.silva@totvs.com.br>
2018-06-28 10:15:33 -0300

    #5248 Ordenação

--- thf-sample-app-conference/src/services/speaker.service.ts
+++ thf-sample-app-conference/src/services/speaker.service.ts
@@ -8,7 +8,7 @@ export class SpeakerService {
   constructor(private thfSync: ThfSyncService) {}
 
   async getSpeakers() {
-    const speakersResponse = await this.thfSync.getModel('Speakers').find().exec();
+    const speakersResponse = await this.thfSync.getModel('Speakers').find().sort('name').exec();
     return speakersResponse.items;
   }
 
